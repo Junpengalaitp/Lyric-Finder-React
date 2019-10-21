970f83848120890b27be65771553055ac5eb1903
hejp009 <hejunpeng2012@hotmail.com>
2019-10-21 15:55:41 +0800

    top 10 tracks showing

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Consumer } from '../../context'
+import Spinner from '../layout/Spinner'
+import Track from './Track';
 
 const Tracks = () => {
     return (
         <Consumer>
             {value => {
-                console.log(value);
-                return <h1>Tracks</h1>
+                const { trackList, heading } = value
+                if (trackList === undefined || trackList.length === 0) {
+                    return <Spinner />
+                } else {
+                    return (
+                        <Fragment>
+                            <h3 className="text-center mb-4">{heading}</h3>
+                            <div className="row">
+                                {trackList.map(item => (
+                                    <Track key={item.track.track_id} track={item.track} />
+                                ))}
+                            </div>
+                        </Fragment>
+                    ) 
+                }
             }}
         </Consumer>
     )
