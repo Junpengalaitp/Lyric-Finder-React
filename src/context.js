e0f25ca50b0c34ad899c8713ba4245eeb8cbe4e4
hejp009 <hejunpeng2012@hotmail.com>
2019-10-21 15:26:13 +0800

    api working

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Context = React.createContext()
 
@@ -10,6 +11,12 @@ export const Provider = (props) => {
     ])
     
     const [heading, setHeading] = useState("Top 10 tracks");
+
+    useEffect(() => {
+        axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err))
+    }, [])
 
     return (
         <Context.Provider value={{trackList, heading}}>
