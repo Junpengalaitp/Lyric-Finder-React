4296f353f7769f7c1ac5f59254cedcccf61cdb23
hejp009 <hejunpeng2012@hotmail.com>
2019-10-21 17:15:30 +0800

    lyrics page complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 import { Provider } from './context';
+import Lyrics from './components/layout/Lyrics';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <div className='container'>
             <Switch>
               <Route exact path='/' component={Index}/>
+              <Route exact path='/lyrics/track/:id' component={Lyrics} />
             </Switch>
           </div>
         </Fragment>
